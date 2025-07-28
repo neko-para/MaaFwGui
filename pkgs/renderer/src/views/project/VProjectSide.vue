2b9a44377f98583e8ec0084ba6809618feb5cd9e
nekosu <liao.hy@outlook.com>
2025-07-28 21:16:36 +0800

    chore: remove useless dialog

--- pkgs/renderer/src/views/project/VProjectSide.vue
+++ pkgs/renderer/src/views/project/VProjectSide.vue
@@ -17,84 +17,9 @@ import {
 const router = useRouter()
 
 const { projectId } = useProject()
-
-const showDlg = ref(false)
-const waitDlg = ref<(v: boolean) => void>(() => {})
-const projectType = ref<'external' | 'githubRelease'>('external')
-const projectTypeOptions: SelectMixedOption[] = [
-    {
-        value: 'external',
-        label: '外部项目'
-    },
-    {
-        value: 'githubRelease',
-        label: 'Github 发布'
-    }
-]
-
-const processing = ref(false)
-
-const githubUrl = ref('')
-const progress = ref('')
-
-async function newProject() {
-    showDlg.value = true
-    processing.value = false
-    if (
-        !(await new Promise<boolean>(resolve => {
-            waitDlg.value = resolve
-        }))
-    ) {
-        showDlg.value = false
-        return
-    }
-
-    processing.value = true
-
-    switch (projectType.value) {
-        case 'external':
-            await requestNewExternalProject()
-            break
-        case 'githubRelease':
-            if (
-                !githubUrl.value ||
-                !/^https?:\/\/github.com\/[^/]+\/[^/]+(?:.git|\/)?$/.test(githubUrl.value)
-            ) {
-                break
-            }
-            await requestNewGithubReleaseProject(githubUrl.value)
-            return
-    }
-    showDlg.value = false
-}
 </script>
 
 <template>
-    <n-modal :show="showDlg" @update:show="() => waitDlg(false)">
-        <n-card title="添加项目" class="max-w-2/3" role="dialog">
-            <div class="flex flex-col gap-2">
-                <n-select
-                    v-model:value="projectType"
-                    :options="projectTypeOptions"
-                    size="small"
-                ></n-select>
-
-                <n-input
-                    v-if="projectType === 'githubRelease'"
-                    placeholder="https://github.com/your/repo"
-                    v-model:value="githubUrl"
-                    size="small"
-                ></n-input>
-
-                <span v-if="progress.length"> {{ progress }} </span>
-            </div>
-
-            <template #footer>
-                <n-button @click="waitDlg(true)" :loading="processing"> 添加 </n-button>
-            </template>
-        </n-card>
-    </n-modal>
-
     <l-generic-side
         title="项目列表"
         :items="projectInfo"
@@ -119,7 +44,7 @@ async function newProject() {
             >
                 GH
             </m-button>
-            <m-button :action="newProject" use-loading> 导入 </m-button>
+            <m-button :action="requestNewExternalProject" use-loading> 导入 </m-button>
         </template>
 
         <template #itemEntry="{ item: project }">
