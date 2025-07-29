b765cf2bf2ec82d49a91290ac1b7a09021ca8b37
dongwlin <dongwlin@foxmail.com>
2025-07-29 22:28:51 +0800

    fix: enhance profile name display with truncation and alignment

--- pkgs/renderer/src/views/profile/VProfileSide.vue
+++ pkgs/renderer/src/views/profile/VProfileSide.vue
@@ -38,7 +38,7 @@ const { profileId } = useProfile()
 
         <template #itemEntry="{ item: profile }">
             <span
-                :class="`text-base ${profile.id === profileId ? 'font-bold text-[#36ad6a]' : ''}`"
+                :class="`text-base w-32 truncate text-left ${profile.id === profileId ? 'font-bold text-[#36ad6a]' : ''}`"
             >
                 {{ profile.name === '' ? '<未命名方案>' : profile.name }}
             </span>
