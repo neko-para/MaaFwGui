6e32ca71bd75c9a9286b7aa54b20bf937282a142
nekosu <liao.hy@outlook.com>
2025-08-06 22:09:15 +0800

    feat: track bottom

--- pkgs/renderer/src/components/MLogPanel.vue
+++ pkgs/renderer/src/components/MLogPanel.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { NScrollbar } from 'naive-ui'
-import { computed } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
+import { nextTick } from 'vue'
 
 import type { LaunchCache } from '@/states/launch'
 
@@ -10,6 +11,9 @@ const props = defineProps<{
     stageName: string
     cache: LaunchCache
 }>()
+
+const outsideEl = ref<HTMLDivElement | null>(null)
+const containerEl = ref<HTMLDivElement | null>(null)
 
 const title = computed(() => {
     if (!props.cache.activeOutput) {
@@ -41,17 +45,46 @@ function resetFollow() {
     props.cache.activeChanged = false
     props.cache.activeOutput = props.cache.latestOutput
 }
+
+const isTrackingBottom = ref(true)
+
+function onScroll(ev: Event) {
+    if (containerEl.value) {
+        isTrackingBottom.value =
+            containerEl.value.clientHeight + containerEl.value.scrollTop + 10 >=
+            containerEl.value.scrollHeight
+    }
+}
+
+watch(
+    () => logs.value.length,
+    () => {
+        if (isTrackingBottom.value) {
+            nextTick(() => {
+                if (containerEl.value) {
+                    containerEl.value.scrollTop = containerEl.value.scrollHeight
+                }
+            })
+        }
+    }
+)
+
+onMounted(() => {
+    nextTick(() => {
+        containerEl.value = outsideEl.value?.querySelector('div.n-scrollbar-container') ?? null
+    })
+})
 </script>
 
 <template>
-    <div class="h-80 mt-auto flex flex-col gap-2">
+    <div ref="outsideEl" class="h-80 mt-auto flex flex-col gap-2">
         <div class="flex items-center justify-between gap-2">
             <span class="text-xl"> {{ title }} </span>
             <m-button v-if="cache.activeChanged" @action="resetFollow"> 跟随 </m-button>
         </div>
-        <n-scrollbar class="p-2 flex flex-col">
+        <n-scrollbar class="p-2 flex flex-col" @scroll="onScroll">
             <div class="flex flex-col gap-0.5">
-                <span v-for="(output, idx) of logs" :key="idx" class="wrap-anywhere">
+                <span v-for="(output, idx) of logs" :key="idx" class="wrap-anywhere select-text">
                     {{ output }}
                 </span>
             </div>
